ce91f1f42d3702e2d41f8881cf9c75d9d7a273f3
Ricardo199 <ricardo1529@gmail.com>
2020-03-03 10:47:03 -0600

    commit

--- php/procesamiento.php
+++ php/procesamiento.php
@@ -3,14 +3,14 @@ if(isset($_REQUEST['cmdEnviar'])){
     $nombres= $_REQUEST['nombres'];
     $apellidos= $_REQUEST['apellidos'];
     $fechaNacimiento= $_REQUEST['fechaNacimiento'];
-    //$sueldo= $_REQUEST['sueldo'];
+    $sueldo= $_REQUEST['sueldo'];
     $pais= $_REQUEST['pais'];
     $preferencias= $_REQUEST['preferencias'];
     $direccion= $_REQUEST['direccion'];
     echo "Informacion recolectada<br>";
-    echo "Nombre: ".$nombres."<br>Apellidos: ".$apellidos."<br> Fecha de nacimiento: ".$fechaNacimiento."<br>Preferencias: <br>";
-    foreach($preferencias as $pre){ 
-        echo $pre." ";
+    echo "Nombre: ".$nombres."<br>Apellidos: ".$apellidos."<br> Fecha de nacimiento: ".$fechaNacimiento."<br>Sueldo: ".$sueldo."<br>Preferencias: <br>";
+    foreach($preferencias as $pre){
+        echo "<ul><li>".$pre."</li></ul>";
     };
     echo "<br>Dirección: ".$direccion;
 }
